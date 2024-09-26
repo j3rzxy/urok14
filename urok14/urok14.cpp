6b30d509988454fb08a49f87a768f392c0b99a80
j3rzxy <j3rzxy@DESKTOP-RMFVR0G>
2024-09-26 10:02:34 +0300

    Добавьте файлы проекта.

---
+++ urok14/urok14.cpp
@@ -0,0 +1,51 @@
+﻿#include<windows.h>
+#include<string>
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    int f;
+    int a, b;
+    int x, y, z;
+    cout << "Что вы хотите проверить?\n";
+    cout << "1 - Прямоугольник или квадрат\n";
+    cout << "2 - Треугольник\n";
+    cout << "Введите 1 или 2: ";
+    cin >> f;
+    if (f == 1) {
+        cout << "Введите 2 стороны прямоугольника:\n";
+        cin >> a;
+        cin >> b;
+        if (a == b) {
+            cout << "Это квадрат\n";
+        }
+        else {
+            cout << "Это прямоугольник\n";
+        }
+    }
+    else
+        if (f == 2) {
+            cout << "Введите 3 стороны треугольника:\n";
+            cin >> x;
+            cin >> y;
+            cin >> z;
+            if (x == y == z) {
+                cout << "Это равносторонний треугольник\n";
+            }
+            else
+                if (x == y or y == z or x == z) {
+                    cout << "Это равнобедренный треугольник\n";
+                }
+            else {
+                cout << "Это разносторонний треугольник\n";
+            }
+    }
+
+        else {
+            cout << "Ошибка!\n";
+        }
+    return 0;
+}
